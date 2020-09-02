8430f58b9a4f1cf88d9df97771d2e20cee4cd824
github username <github email address>
2020-09-02 13:35:23 -0400

    works on login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:email])
-        if @user && @user.authenticate(params[:password])
+        binding.pry
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to controller: 'users', action: 'home'
         else 
