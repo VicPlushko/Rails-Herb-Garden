92fc0659522fe6a519d68a3e74f5dbafe306b986
github username <github email address>
2020-09-02 12:38:17 -0400

    adds Login functionality, and a logout link to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
-    
+    protect_from_forgery with: :exception
+    helper_method :current_user, :logged_in?, :require_login
 
     def home
     end
+
+    private
+
+    
+      def logged_in?
+        current_user != nil
+      end
+    
+      def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+    
+      def require_login
+        redirect_to "/" unless session.include? :user_id
+      end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to controller: 'users', action: 'home'
+        else 
+            flash[:alert] = "The email or password provided is incorrect"
+            redirect_to login_path
+        end 
+
+    end 
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to root_path
+    end
+
 end
